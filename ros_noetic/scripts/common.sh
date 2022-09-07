81aa634af79a08e4412fd6ff826839e1c9492489
yilei_fan <yilei_fan@outlook.com>
2022-09-07 17:02:00 +0200

    feature: add ros_noetic_env

---
+++ ros_noetic/scripts/common.sh
@@ -0,0 +1,4 @@
+DOCKER_IMAGE=ros_noetic_ws:dev
+echo "DOCKER_IMAGE: $DOCKER_IMAGE"
+WORKDIR=/home/ros_noetic_ws
+echo "WORKDIR: $WORKDIR"
---
+++ ros_noetic/scripts/docker_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PROJECT_DIR=$(dirname $(cd $(dirname $BASH_SOURCE); pwd))
+source $PROJECT_DIR/"scripts/common.sh"
+
+docker run -it --rm --net=host -v $PROJECT_DIR/src:$WORKDIR/src -v $PROJECT_DIR/input:$WORKDIR/input -v $PROJECT_DIR/output:$WORKDIR/output $DOCKER_IMAGE
+#docker run -it --rm --net=host $DOCKER_IMAGE
