9d7c4b9546c83e11422c6fcc1629b11332b1d643
yilei_fan <yilei_fan@outlook.com>
2022-09-11 03:01:57 +0200

    modify ros noetic env

--- ros_noetic/scripts/docker_build.sh
+++ ros_noetic/scripts/docker_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 PROJECT_DIR=$(dirname $(cd $(dirname $BASH_SOURCE); pwd))
-source $PROJECT_DIR/"scripts/common.sh"
+source "$PROJECT_DIR/scripts/common.sh"
 
 docker build $PROJECT_DIR -t $DOCKER_IMAGE
